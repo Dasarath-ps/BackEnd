e41e3b24f0f891098dd363399f8e8d30046b2069
Sreekanth2440 <sreekanthps735@gmail.com>
2026-09-03 19:58:17 +0530

    Fast API main python function

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print ("Hello")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI is working!"}
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello from FastAPI"}
+
+
+@app.get("/user/{name}")
+def get_user(name: str):
+    return {"name": name}
